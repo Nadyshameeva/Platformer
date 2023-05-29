271c43208e18ac1189b72bb96543368dabb8aa2d
Nadyshameeva <shameeva.nadya24@gmail.com>
2023-05-29 20:10:34 +0700

    hello :)

--- Hitbox.gd
+++ Hitbox.gd
@@ -10,12 +10,6 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Spikes_body_entered(body):
+func _on_Hitbox_body_entered(body):
 	if body is Player:
 		get_tree().reload_current_scene()
